040a47bba95ff526c750a48b3d02dee8a0a3c378
Bruno Berval <brunoberval@hotmail.com>
2024-06-04 16:32:16 -0300

    feat: update handle structure to separated subpackage

--- router/routes.go
+++ router/routes.go
@@ -1,8 +1,7 @@
 package router
 
 import (
-	"net/http"
-
+	"github.com/BrunoBerval/gopportunities/handler"
 	"github.com/gin-gonic/gin"
 )
 
@@ -10,39 +9,14 @@ func InitializeRoutes(router *gin.Engine) {
 	// Define uma rota
 	v1 := router.Group("/api/v1")
 	{
-		v1.GET("/opening", func(c *gin.Context) {
-			// Adicione aqui a lógica do handler
-			c.JSON(http.StatusOK, gin.H{
-				"message": "GET opening",
-			})
-		})
+		v1.GET("/opening", handler.ShowOpeningHandler)
 
-		v1.POST("/opening", func(c *gin.Context) {
-			// Adicione aqui a lógica do handler
-			c.JSON(http.StatusOK, gin.H{
-				"message": "POST opening",
-			})
-		})
+		v1.POST("/opening", handler.CreateOpeningHandler)
 
-		v1.DELETE("/opening", func(c *gin.Context) {
-			// Adicione aqui a lógica do handler
-			c.JSON(http.StatusOK, gin.H{
-				"message": "DELETE opening",
-			})
-		})
+		v1.DELETE("/opening", handler.DeleteOpeningHandler)
 
-		v1.PUT("/opening", func(c *gin.Context) {
-			// Adicione aqui a lógica do handler
-			c.JSON(http.StatusOK, gin.H{
-				"message": "PUT opening",
-			})
-		})
+		v1.PUT("/opening", handler.UpdateOpeningHandler)
 
-		v1.GET("/openings", func(c *gin.Context) {
-			// Adicione aqui a lógica do handler
-			c.JSON(http.StatusOK, gin.H{
-				"message": "GET openings",
-			})
-		})
+		v1.GET("/openings", handler.ListOpeningsHandler)
 	}
 }
