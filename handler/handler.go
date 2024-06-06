d8869b759fa6bdcf0519008523727a4718f5b746
Bruno Berval <brunoberval@hotmail.com>
2024-06-06 16:06:07 -0300

    feat: add schemas and config logger

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,15 @@
 package handler
+
+import (
+	"gorm.io/gorm"
+)
+
+type Opening struct {
+	gorm.Model
+	Role     string
+	Company  string
+	Location string
+	Remote   bool
+	Link     string
+	Salary   int64
+}
--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/BrunoBerval/gopportunities/router"
+import (
+	"github.com/BrunoBerval/gopportunities/config"
+	"github.com/BrunoBerval/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
-	//Initialize router
+	logger = config.GetLogger()
+	// Iniciar as configurações
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
+	//Iniciar o router
 	router.Initialize()
 }
