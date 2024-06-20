0ef9d205de665aaacb78d8f36575197b87ffcd43
Bruno Berval <brunoberval@hotmail.com>
2024-06-20 18:57:02 -0300

    docs: add documentation for Create, Show, Update, Delete and List openings

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @Basepath /api/v1
+
+// @Summary List openings
+// @Description Show a list of all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /openings [get]
 func ListOpeningsHandler(c *gin.Context) {
 	openings := []schemas.Opening{}
 	if err := db.Find(&openings).Error; err != nil {
